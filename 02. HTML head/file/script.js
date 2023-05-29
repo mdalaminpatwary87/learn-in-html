7e3c7cbf6268ea85778e9cf6c1904f15dde541ed
mdalaminpatwary87 <mdalaminpatwary87@gmail.com>
2023-05-29 19:28:42 +0600

    initial commit

---
+++ 02. HTML head/file/script.js
@@ -0,0 +1,5 @@
+var changeHeading = () => {
+    var heading = document.querySelector('h1');
+    heading.style.color = "green";
+    heading.style.textAlign = "center";
+};
---
+++ 18. HTML in javascript/file/01. script.js
@@ -0,0 +1,4 @@
+document.getElementById("demo").innerHTML="Hello Javascript";
+document.getElementById("demo").style.color="red";
+document.getElementById("demo").style.fontSize="20px";
+document.getElementById("demo").style.backgroundColor="blue";
